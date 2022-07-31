8061ae1fc62012bcc16ba7b3e596179d5ac993ea
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2022-07-31 11:58:08 +0530

    implement List View in SwiftUI

---
+++ SwiftUIList/SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Mahesh Kulkarni on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
